c88f0cd4e2f8f1c5927ffea075faa1b3ed98c778
geetha <geetharanib70@gmail.com>
2025-11-06 12:26:25 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -35,7 +35,7 @@ resource "terraform_data" "catalogue" {
   provisioner "remote-exec" {
     inline = [
          "chmod +x /tmp/bootstrap.sh",
-         "sudo sh /tmp/bootstrap.sh catalogue"
+         "sudo sh /tmp/bootstrap.sh ${var.environment}"
         # "sudo sh /tmp/bootstrap.sh "
         #"echo hello world"
     ]
