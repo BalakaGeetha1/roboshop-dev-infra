650adf0d5b5ba9ae7a3f503f0784ef5b0d27567d
geetha <geetharanib70@gmail.com>
2025-11-07 09:18:39 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -178,6 +178,7 @@ resource "aws_route53_record" "mongodb"{
   type = "A"
   ttl = 1
   records = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "redis"{
@@ -186,6 +187,7 @@ resource "aws_route53_record" "redis"{
   type = "A"
   ttl = 1
   records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "mysql"{
@@ -194,6 +196,7 @@ resource "aws_route53_record" "mysql"{
   type = "A"
   ttl = 1
   records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "rabbitmq"{
@@ -202,4 +205,5 @@ resource "aws_route53_record" "rabbitmq"{
   type = "A"
   ttl = 1
   records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
 }
