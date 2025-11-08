4378502b7ee3d910feae7019d7910197b38c7128
geetha <geetharanib70@gmail.com>
2025-11-08 23:31:03 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -17,7 +17,7 @@ local.common_tags,
 }
 
 #backend alb listening on port no 80
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "backend_alb" {
   load_balancer_arn = aws_lb.backend_alb.arn
   port              = "80"
   protocol          = "HTTP"
@@ -41,8 +41,9 @@ resource "aws_route53_record" "backend_alb" {
 
   alias {
     # these are ALB details, not our domain details
-    name                   = aws_elb.backend_alb.dns_name
-    zone_id                = aws_elb.backend_alb.zone_id
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
     evaluate_target_health = true
   }
 }
+
