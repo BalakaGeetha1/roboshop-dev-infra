24373530355ea355b2dc585bf56eac3a6477a298
geetha <geetharanib70@gmail.com>
2025-11-08 21:46:26 +0530

    terraform

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -2,8 +2,9 @@
 
 #growing /home volume for terraform purpose
 growpart /dev/nvme0n1 4
+#lvextend -L +30G /dev/mapper/rootVol
 lvextend -L +30G /dev/RootVG/rootVol
-xfs-growfs /home
+xfs_growfs /home
 
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
