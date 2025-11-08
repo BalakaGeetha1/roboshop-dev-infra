3290336af87afa0613498d075cc3a27e697ae3f4
geetha <geetharanib70@gmail.com>
2025-11-08 22:48:04 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -32,3 +32,17 @@ resource "aws_lb_listener" "front_end" {
     }
   }
 }
+
+
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name    = "*.backend-alb-${var.environment}.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    # these are ALB details, not our domain details
+    name                   = aws_elb.backend_alb.dns_name
+    zone_id                = aws_elb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+}
--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,12 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z09725051OXQ4HS6KAP4"
+}
+
+variable "domain_name" {
+    default = "gdaws86s.fun"
+}
+
